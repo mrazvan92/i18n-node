4a2d23985a2cf60d58bec372ab951a5182aed691
Razvan Moldovan <razvan.moldovan@arobs.com>
2015-11-23 14:05:09 +0200

    symlinks handling
    Signed-off-by: Razvan Moldovan <razvan.moldovan@arobs.com>

--- i18n.js
+++ i18n.js
@@ -77,7 +77,7 @@ i18n.configure = function i18nConfigure(opt) {
     // auto reload locale file
     if (autoReload) {
         // watch changes of locale files (it's called twice because fs.watch is still unstable)
-        fs.watch(directory, function (event, filename) {
+        watchFile(directory, function (event, filename) {
             var re = new RegExp(extension + '$');
             if (filename && filename.match(re)) {
                 var locale = filename.replace(re, '');
@@ -753,6 +753,30 @@ function getStorageFilePath(locale) {
     logDebug('will write to ' + filepath);
   }
   return filepath;
+}
+
+/**
+ * Helper for watchFile, also handling symlinks
+ */
+function watchFile(path, callback) {
+    // Check if it's a link
+    fs.lstat(path, function(err, stats) {
+        if(err) {
+            // Handle errors
+            return callback(err);
+        } else if(stats.isSymbolicLink()) {
+            // Read symlink
+            fs.readlink(path, function(err, realPath) {
+                // Handle errors
+                if(err) return callback(err);
+                // Watch the real file
+                fs.watch(realPath, callback);
+            });
+        } else {
+            // It's not a symlink, just watch it
+            fs.watch(path, callback);
+        }
+    });
 }
 
 /**
